f904c2403cdf1151caf4983db1eff550ebb70de2
zac <zac@egghead.io>
2020-06-03 11:54:30 -0400

    Add APIGateway

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,9 +1,10 @@
 exports.handler = async function (event) {
   console.log("event: ", JSON.stringify(event, null, 2));
+  console.log("is production?", process.env.isProduction);
 
   return {
     statusCode: 200,
     headers: {"Content-Type": "text/plain"},
-    body: "Hello, egghead friends!",
+    body: `Hello, egghead friends! You've hit ${event.path}`,
   }
 }
