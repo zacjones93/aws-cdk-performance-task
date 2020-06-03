711ecff3ebac9ec2f7d0533b65a8585d7f4d1352
zac <zac@egghead.io>
2020-06-03 17:08:15 -0400

    delete typescript build files

--- lambda/emailCapture.d.ts
+++
@@ -1,8 +0,0 @@
-declare const AWS: typeof import("aws-sdk");
-declare const tableName: string;
-declare const dynamo: import("aws-sdk/clients/dynamodb").DocumentClient;
-declare const createResponse: (body: string | AWS.DynamoDB.DocumentClient.ItemList, statusCode?: number) => {
-    statusCode: number;
-    body: string;
-};
-declare const getAllEmails: () => Promise<import("aws-sdk/lib/request").PromiseResult<import("aws-sdk/clients/dynamodb").DocumentClient.ScanOutput, import("aws-sdk").AWSError>>;
--- lib/backend.d.ts
+++
@@ -1,6 +0,0 @@
-import * as cdk from "@aws-cdk/core";
-import * as lambda from "@aws-cdk/aws-lambda";
-export declare class Backend extends cdk.Construct {
-    readonly handler: lambda.Function;
-    constructor(scope: cdk.App, id: string);
-}
